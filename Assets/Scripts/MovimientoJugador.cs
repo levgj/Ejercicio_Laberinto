c0dc20e937e5c9b9cecd708d4d0dc8bfe84da972
Levgj <127439153+levgj@users.noreply.github.com>
2025-10-01 23:57:54 +0100

    Creación MovimientoJugador
    Creación del script MovimientoJugador, y de los public float que representan el movimiento en los ejes.

---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public float movimientoEjeX;
+    public float movimientoEjeY;
+    public float movimientoEjeZ;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        Debug.Log("Hola mundo"); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
