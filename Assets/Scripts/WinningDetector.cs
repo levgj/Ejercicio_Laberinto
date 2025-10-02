391f2b9383254b3d8f00a6e3b27bbf71dc646266
levgj <127439153+levgj@users.noreply.github.com>
2025-10-02 11:28:43 +0200

    Tiempo de juego y Pantalla de Victoria
    Se ha creado la pantalla de Victoria, y se ha añadido un temporizador del juego que se muestra aún en la consola únicamente.

--- Assets/Scripts/WinningDetector.cs
+++ Assets/Scripts/WinningDetector.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class WinningDetector : MonoBehaviour
 {
+    [SerializeField]
+    GameObject pantallaDeVictoria;
+
+    float gameTime = 0f;
+    bool isPlaying = true;
+
+    private void Update()
+    {
+        if (isPlaying == true)
+        {
+            gameTime = gameTime + Time.deltaTime;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Se llegó a la meta");
+            pantallaDeVictoria.SetActive(true);
+            other.GetComponent<MovimientoJugador>().enabled = false;
+            isPlaying = false;
+            Debug.Log(gameTime);
         }
     }
 
